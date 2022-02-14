1b5ffdc7e830616bf4b42e925dff900860bb8850
Josue <jolesa97@gmail.com>
2022-02-14 17:51:10 -0600

    organizing structure

--- BasisTheory.LuceneSearchingExample/src/Models/Requests/SearchPersonsRequest.cs
+++
@@ -1,9 +0,0 @@
-using Newtonsoft.Json;
-
-namespace BasisTheory.LuceneSearchingExample.Models.Requests;
-
-public class SearchPersonsRequest
-{
-    [JsonProperty("query")]
-    public string Query { get; set; }
-}
